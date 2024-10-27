ff77b1f59d09833d5d0ace22cc8d31c94d0cb6d8
devon4329 <devon4329@proton.me>
2024-10-27 11:40:08 -0600

    Added main function and description
    Added main function and call and description of the program

--- Task1.py
+++ Task1.py
@@ -1,3 +1,16 @@
 """
 CS3080 Project - Task 1
+This program will scrape a website,
+https://www.ef.edu/english-resources/english-vocabulary/top-1000-words/, for the top 1000
+English words. The program will take these words and put them into an Excel sheet. This Excel
+sheet will then be used as input to create a hangman-like word game where the user has a limited
+number of guesses to guess each letter in the random word out of the Excel sheet.
 """
+import requests, bs4
+
+
+
+def main():
+
+if __name__ == "__main__":
+    main()
