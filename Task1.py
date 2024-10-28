f431e06140904a5616dd08f7fcc93ed958451d21
devon4329 <devon4329@proton.me>
2024-10-28 07:51:42 -0600

    select_word
    select_word function successfully pulls a random word from an excel file. Tested with Test.xlsx file and was successful.

--- Task1.py
+++ Task1.py
@@ -6,11 +6,23 @@ English words. The program will take these words and put them into an Excel shee
 sheet will then be used as input to create a hangman-like word game where the user has a limited
 number of guesses to guess each letter in the random word out of the Excel sheet.
 """
-import requests, bs4
+import requests, bs4, openpyxl, random
 
+file = "Test.xlsx"
 
+# select_word function to randomly select a word from an Excel file
+def select_word(file):
+    wb = openpyxl.load_workbook(file)
+    sheet = wb['Sheet 1']
 
-def main():
+    word = [cell.value for cell in sheet['A'] if cell.value is not None]
 
-if __name__ == "__main__":
-    main()
+    selected_word = random.choice(word)
+    return selected_word
+
+print(select_word(file))
+
+#def main():
+
+#if __name__ == "__main__":
+    #main()
