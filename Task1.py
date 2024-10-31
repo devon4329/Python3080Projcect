14f87c30992cad17d3e1b726ad423a3d2208c233
WastedTitanium <62776599+WastedTitanium@users.noreply.github.com>
2024-10-31 08:58:49 -0600

    Add guessed_wrod functionality and Prettify code

--- Task1.py
+++ Task1.py
@@ -2,23 +2,33 @@
 # CS3080 Python Programming
 # Task 1
 
-""" Task 1 - Simple Word Game.
-This program will scrape a website, 
-https://www.ef.edu/english-resources/english-vocabulary/top-1000-words/, for the top 1000 English words. 
-The program will take these words and put them into an excel sheet. 
-This excel sheet will then be used as input to create a hangman-like word game where the 
-user has a limited number of guesses to guess each letter in the random word out of the excel sheet.
+"""Task 1 - Simple Word Game.
+
+This program will scrape a website,
+https://www.ef.edu/english-resources/english-vocabulary/top-1000-words/,
+for the top 1000 English words.
+The program will take these words and put them into an excel sheet.
+This excel sheet will then be used as input to createa hangman-like
+word game where the user has a limited number of guesses to guess
+each letter in the random word out of the excel sheet.
 
 """
 
-import requests, bs4, openpyxl, random, textwrap
+import requests
+import bs4
+import openpyxl
+import random
+import textwrap
 
 FILE = "Task1.xlsx"
 INCORRECT_GUESSES = 3
 
+
 class Game():
+    """Represents the game that the user plays."""
 
     def __init__(self):
+        """Each game has data about the word and the guesses."""
         self.word = self.select_word(FILE)
         self.word_length = len(self.word)
         self.letters_guessed = []
@@ -27,6 +37,7 @@ class Game():
         self.guessed_word = False
 
     def select_word(self, file):
+        """Selecet a random word from word_bank."""
         wb = openpyxl.load_workbook(file)
         sheet = wb['Sheet']
 
@@ -34,9 +45,9 @@ class Game():
 
         selected_word = random.choice(word)
         return selected_word
-        
 
     def display_word(self):
+        """Display the partially reealed word."""
         for char in self.word:
             if char is not None:
                 if char in self.letters_guessed:
@@ -44,21 +55,25 @@ class Game():
                 else:
                     print("_", end=' ')
         print()
-    
+
     def guess_letter(self):
+        """Get user input for a letter guess."""
         valid = False
 
+        # Keep asking for a valid guess, doing some error checking first
+        # Then determine if the letter has been guessed already
+        # Then determine if the guess is in the word or not
         while not valid:
             guess = input(f"Guess {self.guess_index} (Incorrect Guesses {self.incorrect_guesses}): ").lower()
             if len(guess) == self.word_length:
                 if guess == self.word:
                     print("You guessed the word!")
-                    self.guessed_word == True
+                    self.guessed_word is True
                     break
                 else:
                     print(f"The word is not {guess}")
                     self.guess_index += 1
-                    self.incorrect_guesses +=1
+                    self.incorrect_guesses += 1
                     break
             if len(guess) > 1 or len(guess) < 1:
                 print("ERROR: Please input only one letter or length of word")
@@ -75,14 +90,12 @@ class Game():
                 self.guess_index += 1
                 self.letters_guessed.append(guess)
                 valid = True
-                break
             else:
                 print(f"{guess} is in the word!")
                 self.letters_guessed.append(guess)
                 self.guess_index += 1
                 valid = True
-                break
-        
+
 
 def get_words():
     """Get the words off of the internet."""
@@ -90,16 +103,23 @@ def get_words():
         response = requests.get('https://www.ef.edu/english-resources/english-vocabulary/top-1000-words/')
         response.raise_for_status()
     except Exception as e:
+        # If requests can not connect to the link, display error and quit program
+        # Functionality could be made to check if the user already has the sheet
         print(f"ERROR: {e}\nCheck Internet Connection")
         quit()
 
+    # Create an html parser
     soup = bs4.BeautifulSoup(response.text, 'html.parser')
 
+    # Find the div that contains the words
     div = soup.find('div', {'class': 'field-item even'})
 
+    # Grab the paragraphs in the div
     paragraphs = div.find_all('p')
 
+    # The words are stored in the 2nd <p> in <div>
     words = paragraphs[1].text
+    # Put the words into a list
     words = words.split()
 
     return words
@@ -109,10 +129,11 @@ def create_sheet(words):
     """Create the word bank spreadsheet."""
     wb = openpyxl.Workbook()
     sheet = wb.active
-    
+
+    # Add words to the 'A' Column of the sheet
     for row, word in enumerate(words, start=1):
         sheet.cell(row, 1, word)
-    
+
     wb.save('Task1.xlsx')
 
 
@@ -121,17 +142,21 @@ def main_game(game):
     print("Welcome to Nick and Devon's Hangman Game!")
     print(textwrap.fill(f"You will have {INCORRECT_GUESSES} guesses to reveal the hidden word selected from the top 1000 English words.", 40))
 
+    # Jump out of the loop if the player reached the max number of
+    # incorrect guesses or if the word is guessed
     while game.incorrect_guesses < INCORRECT_GUESSES and not game.guessed_word:
         game.display_word()
         game.guess_letter()
-        # TODO: After every guess, check if every letter of the word is in the letters
-        # guessed list. Thus, the word has been guessed. This can be done with a boolean flag
-        # to stop the while loop along with 
+        if all(char in game.letters_guessed for char in game.word):
+            game.guessed_word = True
 
-    # End game (Could be made neater)
+    # Basic end game to display if the user won or lost
     if not game.guessed_word:
+        print("\nGame Over!")
         print(f"The word was {game.word}")
-    
+    else:
+        print(f"\nYou correctly guessed the word, {game.word} in {game.guess_index} guesses!")
+
 
 def main():
     """Run Main Function."""
