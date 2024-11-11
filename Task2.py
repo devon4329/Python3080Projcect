b52e16e28b8a5f0101e85a7319bf8114ff6eb536
devon4329 <devon4329@proton.me>
2024-11-10 18:53:36 -0700

    Complete?

--- Task2.py
+++ Task2.py
@@ -6,48 +6,120 @@ and obtain the top three contributors (authors or researchers) in a conference f
 (2022, 2023, and 2024). The results will be saved into an Excel spreadsheet that shows how many times each
 author contributed to a paper separated by the year.
 """
+from venv import create
+
 import openpyxl
+from numpy.core.defchararray import center
+from openpyxl.styles import Alignment, Font
 import bs4
 import requests
 
-
-test_dict1 = dict({'bill': 2, "jill": 3, "frank": 1})
-test_dict2 = dict({'bill': 8, "jill": 7, "frank": 9, "hank": 10})
-test_dict3 = dict({'bill': 8, "jill": 7, "frank": 9, "mary": 10})
-
+LINK_2022 = r'https://openaccess.thecvf.com/CVPR2022?day=all'
+LINK_2023 = r'https://openaccess.thecvf.com/CVPR2023?day=all'
+LINK_2024 = r'https://openaccess.thecvf.com/CVPR2024?day=all'
 
 
+def find_contributors(link):
+    try:
+        response = requests.get(link)
+        response.raise_for_status()
+
+    except Exception as e:
+        print(f"Error {e}\nCheck Internet Connection")
+        quit()
+
+    soup = bs4.BeautifulSoup(response.text, 'html.parser')
+
+    div = soup.find('div', {'id' : 'content'})
+
+    authors = list(div.select('input[type=hidden]'))
+
+    author_counts = {}
+
+    for element in authors:
+        value = element.get("value")
+
+        if value in author_counts:
+            author_counts[value] += 1
+        else:
+            author_counts[value] = 1
+
+    return author_counts
+
+# Counts Totals of all lists
 def count_totals(dict1, dict2, dict3):
     the_total = {}
-    for key, values in dict1.items():
+
+    def add_totals(key, values):
+
         if key in the_total:
-            the_total[key] += values
+            the_total[key][0].append(values)
+            the_total[key][1] += values
         else:
-            the_total[key] = values
-    for key, values in dict2.items():
-        if key in the_total:
-            the_total[key] += values
+            the_total[key] = [[values], values]
+
+    for dict_in in [dict1, dict2, dict3]:
+        for key, value in dict_in.items():
+            add_totals(key, value)
+
+    sorted_list = sorted_list = sorted([(key, values[0], values[1])
+                                        for key, values in the_total.items()], key=lambda x: x[2], reverse=True)
+
+    return sorted_list
+
+
+
+def create_sheet(aList):
+    top_con = openpyxl.Workbook()
+    print(top_con.sheetnames)
+    sheet = top_con.active
+    sheet.title = "CVF Top 3 Contributors"
+    print(top_con.sheetnames)
+
+    # Add years and total to sheet
+    start_year = 2022
+    for i in range(4):
+        if i < 3:
+            cell2022 = sheet.cell(row=i+2,column=1, value = start_year + i )
+            cell2022.font = Font(bold=True)
+            cell2022.alignment = Alignment(horizontal="center", vertical="center")
         else:
-            the_total[key] = values
-    for key, values in dict3.items():
-        if key in the_total:
-            the_total[key] += values
-        else:
-            the_total[key] = values
+            cell2022 = sheet.cell(row=i+2, column=1, value = "Total")
+            cell2022.font = Font(bold=True)
+            cell2022.alignment = Alignment(horizontal="center")
 
-    return list(the_total.items())
 
-final_count = count_totals(test_dict1, test_dict2, test_dict3)
+    # Add names
+    for i in range(3):
+        cell = sheet.cell(row= 1, column = i+2, value = aList[i][0])
+        cell.alignment = Alignment(horizontal="center", vertical="center")
 
-def sort_and_reverse(mylist):
-    first = len(mylist)
-    for i in range(0, first):
-        for j in range(0, first - i - 1):
-            if mylist[j][1] < mylist[j+1][1]:
-                temp = mylist[j]
-                mylist[j] = mylist[j+1]
-                mylist[j+1] = temp
+    #Add year 2022 values to sheet
+    index = 2
+    for key in aList[:3]:
+        i = 0
+        for value in (key[1]):
+            cell2 = sheet.cell(row=i+2, column=index, value=value)
+            cell2.alignment = Alignment(horizontal="center", vertical="center")
+            i += 1
 
-    return mylist
+        cell2 = sheet.cell(row=5, column = index, value=key[2])
+        cell2.alignment = Alignment(horizontal="center", vertical="center")
+        index += 1
 
-print(sort_and_reverse(final_count))
+    top_con.save('Task2.xlsx')
+
+
+def main():
+    contributors_2022 = find_contributors(LINK_2022)
+    contributors_2023 = find_contributors(LINK_2023)
+    contributors_2024 = find_contributors(LINK_2024)
+
+    totals = count_totals(contributors_2022, contributors_2023, contributors_2024)
+
+    create_sheet(totals)
+
+
+
+if __name__ == "__main__":
+    main()
